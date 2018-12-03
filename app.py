8c548b365e09b6e80c5c24480bf4697855c1af22
Darth-Par <pmicoo@hotmail.com>
2018-12-03 00:36:14 +0000

    new route.

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-print(__name__)
 
+books = [
+    {
+        "name": "Green Eggs and Ham",
+        "price": 7.99,
+        "isbn": 9780
+    },
+    {
+        "name": "The Cat In The Hat",
+        "price": 3.99,
+        "isbn": 9880
+    }
+]
+
+# GET /
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+# GET /books
+@app.route('/books')
+def get_books():
+    return jsonify({ 
+        "books": books
+    })
+
+# Server running.....
 app.run(port=5000)
