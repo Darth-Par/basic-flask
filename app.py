d15f83fb69e634b86ac4efd9ebe3fb63068e54db
Darth-Par <pmicoo@hotmail.com>
2018-12-04 21:22:27 +0000

    GET single book.

--- app.py
+++ app.py
@@ -20,6 +20,19 @@ books = [
 def hello_world():
     return 'Hello World!'
 
+# GET single book
+@app.route('/books/<int:isbn>')
+def get_book_by_isbn(isbn):
+    for book in books:
+        if book['isbn'] == isbn:
+            return jsonify({
+                "book": book
+            })
+        else:
+            return jsonify({
+                "Error": "That book does not exist."
+            })
+
 # GET /books
 @app.route('/books')
 def get_books():
